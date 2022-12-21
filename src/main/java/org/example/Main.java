449270ce74bead297d8f9ffa2f949027edb8b2f4
Obruch Polina <obruchspb@yandex.ru>
2022-12-21 19:35:27 +0300

    polina from Res before PR

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1!");
         System.out.println("7");
+        System.out.println("polina from Res");
     }
 }
