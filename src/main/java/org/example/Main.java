61855fb0c0896a2317130efb3856e82eed9e72f8
Ashot Avetisian <ashot463@mail.ru>
2022-12-21 20:10:29 +0300

    Update Main.java
    ashot from b3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1!");
+        System.out.println("3!");
         System.out.println("7");
+        System.out.println("ashot from d3");
         System.out.println("polina from Res");
         System.out.println("ashot from d2");
     }
